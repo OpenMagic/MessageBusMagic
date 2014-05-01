c46ac0973236ae5d5f14443fe471c68fe8a9237d
Tim Murphy <tim@26tp.com>
2014-05-02 08:06:25 +1000

    Added 'Subscribe & publish messages' specification to 'MessageBus' feature.

---
+++ Projects/MessageBusMagic.Specifications/Features/MessageBus.feature
@@ -0,0 +1,10 @@
+﻿Feature: MessageBus
+	In order to develop decoupled applications
+	As a programmer
+	I want a MessageBus to subscribe to & publish messages 
+
+Scenario: Subscribe & publish messages
+	Given I have a MessageBus
+	When I subscribe to a message
+	And I publish the message
+	Then the message should be received by the subscriber
---
+++ Projects/MessageBusMagic.Specifications/Features/Steps/MessageBusSteps.cs
@@ -0,0 +1,32 @@
+﻿using TechTalk.SpecFlow;
+
+namespace MessageBusMagic.Specifications.Features.Steps
+{
+    [Binding]
+    public class MessageBusSteps
+    {
+        [Given(@"I have a MessageBus")]
+        public void GivenIHaveAMessageBus()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I subscribe to a message")]
+        public void WhenISubscribeToAMessage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I publish the message")]
+        public void WhenIPublishTheMessage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"the message should be received by the subscriber")]
+        public void ThenTheMessageShouldBeReceivedByTheSubscriber()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
