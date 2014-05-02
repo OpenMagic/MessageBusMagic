7f42e23bbbe49a0300d241af05e3d9fbc49d5d35
Tim Murphy <tim@26tp.com>
2014-05-03 07:01:51 +1000

    Added specifications helper.

---
+++ Projects/MessageBusMagic.Specifications/Support/Helpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MessageBusMagic.Specifications.Support
+{
+    internal class Helpers
+    {
+        internal static TException Catch<TException>(Action action) where TException : Exception
+        {
+            try
+            {
+                action();
+                return null;
+            }
+            catch (AggregateException aggregateException)
+            {
+                if (aggregateException.InnerExceptions.Count == 1 && aggregateException.InnerExceptions[0].GetType() == typeof(TException))
+                {
+                    return (TException) aggregateException.InnerExceptions[0];
+                }
+                throw;
+            }
+            catch (TException exception)
+            {
+                return exception;
+            }
+        }
+    }
+}
